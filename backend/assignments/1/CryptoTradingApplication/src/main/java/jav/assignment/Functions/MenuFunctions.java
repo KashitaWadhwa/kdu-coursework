6a0986764db0d5eba7c57ac4bb607fbcfb5e8a99
KashitaWadhwa <kashita.wadhwa@kickdrumtech.com>
2024-01-15 00:45:12 +0530

    changes to files

--- backend/assignments/1/CryptoTradingApplication/src/main/java/jav/assignment/Functions/MenuFunctions.java
+++ backend/assignments/1/CryptoTradingApplication/src/main/java/jav/assignment/Functions/MenuFunctions.java
@@ -21,24 +21,27 @@ class SortByProfit implements Comparator<Trader> {
 public class MenuFunctions {
     public static void getDetailsByNameOrCodeOfCoin() {
         Scanner sc = new Scanner(System.in);
-        Logging.logInfo("1 -> Get Coin details by Name\n2 -> Get Coin details by Symbol");
+        System.out.println("1 -> Get Coin details by Name\n2 -> Get Coin details by Symbol");
         int input = sc.nextInt();
         if(input == 1) {
             sc.nextLine();
-            Logging.logInfo("Enter a name of coin");
+            System.out.println("Enter a name of coin");
             String coinName = sc.nextLine();
-            Logging.logInfo(GlobalData.coinNameAndCoinObject.get(coinName).toString());
+            System.out.println(GlobalData.coinNameAndCoinObject.get(coinName).toString());
         }
         else if(input == 2) {
             sc.nextLine();
 
-            Logging.logInfo(GlobalData.coinSymbolAndCoinObject.size());
-            Logging.logInfo("Enter a symbol of coin");
+            System.out.println(GlobalData.coinSymbolAndCoinObject.size());
+            System.out.println("Enter a symbol of coin");
             String coinSymbol = sc.nextLine();
-            Logging.logInfo(GlobalData.coinSymbolAndCoinObject.get(coinSymbol.toUpperCase()).toString());
+
+//            System.out.println(coinSymbol);
+//            System.out.println(GlobalData);
+            System.out.println(GlobalData.coinSymbolAndCoinObject.get(coinSymbol.toUpperCase()).toString());
         }
         else {
-            Logging.logInfo("Enter a valid choice");
+            System.out.println("Enter a valid choice");
         }
     }
 
@@ -48,10 +51,10 @@ public class MenuFunctions {
                 .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
 
         int counter = 0;
-        Logging.logInfo("Top 50 coins based on the price: ");
+        System.out.println("Top 50 coins based on the price: ");
         for(String symbol : top50Coins.keySet()) {
             counter++;
-            Logging.logInfo("Coin " + counter + ": " + top50Coins.get(symbol).toString());
+            System.out.println("Coin " + counter + ": " + top50Coins.get(symbol).toString());
 
             if(counter == 50)
                 break;
@@ -60,13 +63,13 @@ public class MenuFunctions {
 
     public static void getPortfolioByName() {
         Scanner sc = new Scanner(System.in);
-        Logging.logInfo("Enter name");
+        System.out.println("Enter name");
         String name = sc.nextLine();
-        Logging.logInfo(GlobalData.allTraderList.size());
+        System.out.println(GlobalData.allTraderList.size());
         for(Trader trader : GlobalData.allTraderList) {
 
             if(trader.getFirstName().equals(name)) {
-                Logging.logInfo(trader.toString());
+                System.out.println(trader.toString());
             }
         }
     }
@@ -75,27 +78,27 @@ public class MenuFunctions {
         int total = GlobalData.allTraderList.size();
         for(int i = 0; i < total; i++) {
             if(name.equals(GlobalData.allTraderList.get(i).getFirstName())) {
-                Logging.logInfo("Transaction Profit/Loss is " + GlobalData.allTraderList.get(i).getProfit());
+                System.out.println("Transaction Profit/Loss is " + GlobalData.allTraderList.get(i).getProfit());
                 return;
             }
-            else Logging.logInfo("Trader not Found..!");
+            else System.out.println("Trader not Found..!");
         }
     }
-
+    //1.01,89002738
     public static void TopNBottomNTraders(int n) {
         if(GlobalData.allTraderList.size() < n) {
-            Logging.logInfo("There are less traders than expected");
+            System.out.println("There are less traders than expected");
         }
         else {
             List<Trader> topNTraders = GlobalData.allTraderList.stream().sorted(Collections.reverseOrder(new SortByProfit())).limit(n).collect(Collectors.toList());
-            Logging.logInfo("Top " + n + "Top/Bottom Traders based on Profit");
+            System.out.println("Top " + n + "Top/Bottom Traders based on Profit");
             List<Trader> BottomNTraders = GlobalData.allTraderList.stream().sorted(new SortByProfit()).limit(n).collect(Collectors.toList());
 
             for (Trader trader : topNTraders)
-                Logging.logInfo(trader.toString());
-            Logging.logInfo();
+                System.out.println(trader.toString());
+            System.out.println();
             for (Trader trader : BottomNTraders)
-                Logging.logInfo(trader.toString());
+                System.out.println(trader.toString());
         }
     }
 }
