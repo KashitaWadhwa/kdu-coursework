3c73b481e2fb1645224d224cdb19f01b94995e26
KashitaWadhwa <kashita.wadhwa@kickdrumtech.com>
2024-02-15 02:10:51 +0530

    Adding frontend-homework-7

---
+++ frontend/homework/7/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function() {
+    console.log(`listening on *:${PORT}`);
+});
